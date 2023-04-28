42016ea3c9f147860156961f703912b988a64879
arif-k32 <arif3681@gmail.com>
2023-04-28 08:21:28 +0530

    photos

--- src/app/shared/services/data-supply.service.ts
+++ src/app/shared/services/data-supply.service.ts
@@ -12,7 +12,7 @@ export class DataSupplyService {
       profile_pic: 'https://randomuser.me/api/portraits/women/65.jpg',
       active: true,
       posts: [
-        { post_id: 1, post_link: 'https://loremflickr.com/1200/800?random=123', post_time: '2023-04-26T14:30:00.000Z', caption:"Cuddle time with my furry best friend! #catsofinstagram #caturday" },
+        { post_id: 1, post_link: 'https://etimg.etb2bimg.com/photo/93513128.cms', post_time: '2023-04-26T14:30:00.000Z', caption:"Wandering through ancient streets, lost in time and lost in wonder" },
         { post_id: 2, post_link: 'https://loremflickr.com/1200/800?random=456', post_time: '2023-04-25T09:15:00.000Z' },
         { post_id: 3, post_link: 'https://loremflickr.com/1200/800?random=178', post_time: '2023-04-24T18:45:00.000Z' }
       ]
@@ -22,7 +22,7 @@ export class DataSupplyService {
       profile_pic: 'https://randomuser.me/api/portraits/men/42.jpg',
       active: false,
       posts: [
-        { post_id: 1, post_link: 'https://loremflickr.com/1200/800?random=147', post_time: '2023-04-26T08:00:00.000Z', caption:"Just enjoying the simple pleasures in life. #catlife #cozy" },
+        { post_id: 1, post_link: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/c5/4c/65/caption.jpg?w=1200&h=-1&s=1', post_time: '2023-04-26T08:00:00.000Z', caption:"Sunsets and good vibes, that's what life is all about." },
         { post_id: 2, post_link: 'https://loremflickr.com/1200/800?random=123', post_time: '2023-04-23T11:30:00.000Z' }
       ]
     },
@@ -31,7 +31,7 @@ export class DataSupplyService {
       profile_pic: 'https://randomuser.me/api/portraits/women/87.jpg',
       active: true,
       posts: [
-        { post_id: 1, post_link: 'https://loremflickr.com/1200/800?random=369', post_time: '2023-04-25T17:00:00.000Z', caption:"If you need me, I'll be napping with my kitty. #catnap #snuggles" },
+        { post_id: 1, post_link: 'https://media.nomadicmatt.com/travelnomoney000.jpg', post_time: '2023-04-25T17:00:00.000Z', caption:"Making memories, one destination at a time." },
         { post_id: 2, post_link: 'https://loremflickr.com/1200/800?random=258', post_time: '2023-04-23T08:45:00.000Z' },
         { post_id: 3, post_link: 'https://loremflickr.com/1200/800?random=159', post_time: '2023-04-22T12:15:00.000Z' }
       ]
@@ -41,7 +41,7 @@ export class DataSupplyService {
       profile_pic: 'https://randomuser.me/api/portraits/men/18.jpg',
       active: true,
       posts: [
-        { post_id: 1, post_link: 'https://loremflickr.com/1200/800?random=357', post_time: '2023-04-26T10:30:00.000Z', caption:"This is what pure happiness looks like. #catlove #felinefriendship" },
+        { post_id: 1, post_link: 'https://img.freepik.com/premium-photo/backpack-asian-man-mountain-see-view-panorama-beautiful-nature-landscape-sea-adventure-vacation-travel-leisure-asia-mu-ko-ang-thong-island-national-park-background-thailand_536080-1002.jpg?w=2000', post_time: '2023-04-26T10:30:00.000Z', caption:"Adventure awaits around every corner, embrace the journey" },
         { post_id: 2, post_link: 'https://loremflickr.com/1200/800?random=486', post_time: '2023-04-24T20:00:00.000Z' }
       ]
     }
